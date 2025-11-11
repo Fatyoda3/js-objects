3fac6d23b117fd741437fdd43ac499572e172911
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-11 12:00:18 +0530

    `just another commit `

--- Objects.js
+++ Objects.js
@@ -6,5 +6,18 @@ name-${person.name} ,
 employed-${person.isEmployed}`
 
 };
-const person = { name: 'shivang', age: 22, isEmployed: true && false };
+const person = { name: 'Shivang', age: 22, isEmployed: true && false };
 console.log(formattedDetails.getDetails(person));
+
+const dog = {
+  name: "Jack",
+  age: 12,
+  color: 'hazy',
+  bark: (count) => {
+    for (let index = 0; index < count; index++) {
+      console.log(`woof `);
+    }
+  }
+
+};
+
