833286bf90a053ae6f9079463c538f297724e2fa
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-11 12:54:10 +0530

    `just another coommit  `

--- animate-ball.js
+++ animate-ball.js
@@ -1,33 +1,41 @@
 const BALL = {
   symbol: 'o',
   x: 0, y: 0,
-  xVel: 0, yVel: 0
+  xVel: Math.floor(Math.random() * 2 + 1), yVel: Math.floor(Math.random() * 2 + 1),
+
 };
 
-const CELL = '_';
+const CELL = ' ';
 
-const DIMENSION = 10;
+const DIMENSION = 20;
 
 const generateGrid = (x, y) => {
   const grid = [];
   const row = CELL.repeat(DIMENSION);
 
   for (let i = 0; i < DIMENSION; i++) {
-    grid.push(row);
+    grid.push(row.split(','));
   }
-  console.log(grid.split()[0][0]);
+  console.log(grid[0]);
+  console.log(grid[x][y] = BALL.symbol, 'grid cell at  0 0 ');
+
   return grid.join('\n');
 };
 const delay = (() => { for (let _ = 0; _ < 10e8; _++); });
 
 const animate = () => {
 
-  for (let _ = 0; _ < 500; _++) {
-    // const
-    console.log(generateGrid());
-    delay();
-    console.clear();
-  }
+  // for (let _ = 0; _ < 500; _++) {
+
+  //   BALL.x = (BALL.x + BALL.xVel) % DIMENSION;
+  //   BALL.y = (BALL.y + BALL.yVel) % DIMENSION;
+
+  console.log(generateGrid(BALL.x, BALL.y));
+
+  //   delay();
+
+  //   console.clear();
+  // }
 
 };
 const main = () => {
