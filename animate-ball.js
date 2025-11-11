747eb2b9006d03bca15c7da70b1712eed6970939
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-11 12:45:08 +0530

    `just made some changes `

--- animate-ball.js
+++ animate-ball.js
@@ -1,21 +1,29 @@
-const BALL = 'o';
+const BALL = {
+  symbol: 'o',
+  x: 0, y: 0,
+  xVel: 0, yVel: 0
+};
+
 const CELL = '_';
 
 const DIMENSION = 10;
 
-const generateGrid = () => {
+const generateGrid = (x, y) => {
   const grid = [];
   const row = CELL.repeat(DIMENSION);
+
   for (let i = 0; i < DIMENSION; i++) {
     grid.push(row);
   }
+  console.log(grid.split()[0][0]);
   return grid.join('\n');
 };
 const delay = (() => { for (let _ = 0; _ < 10e8; _++); });
 
 const animate = () => {
-  for (let _ = 0; _ < 500; _++) {
 
+  for (let _ = 0; _ < 500; _++) {
+    // const
     console.log(generateGrid());
     delay();
     console.clear();
